7b0dc18914df22ce14ac2e368f4070055a669659
gfn1218@gmail.com <gfn1218@gmail.com>
2011-02-09 23:23:15 +0800

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      flash[:success] = "welcome to the sample app"
       redirect_to @user
     else
       @title = "Sign up"
