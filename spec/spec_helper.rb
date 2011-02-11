5a4b87a61b7213662e32ee09d222ffd24c8def4c
gfn1218@gmail.com <gfn1218@gmail.com>
2011-02-11 23:56:04 +0800

    Done with Sign in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
   ActiveSupport::Dependencies.clear
+  
+  def test_sign_in(user)
+    controller.sign_in(user)
+  end
 end
 end
 Spork.each_run do
